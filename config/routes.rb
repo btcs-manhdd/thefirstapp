e36bf7a25853ba9ccf3f6888961a8f783a3c795f
btcs-manhdd <manhdd@btc-studio.io>
2023-01-10 13:52:21 +0700

    add_create_books_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/" , to: "static_pages#home"
+  get "/books/new", to: "books#new"
   get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
